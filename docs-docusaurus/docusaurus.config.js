95d487218a5b6d939cea65795bcd023a3824f312
Ali Can Yagiz <2127970+241@users.noreply.github.com>
2025-03-10 13:06:08 +0300

    baseurl updated

--- docs-docusaurus/docusaurus.config.js
+++ docs-docusaurus/docusaurus.config.js
@@ -15,10 +15,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://241.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/aks-troubleshooting/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
